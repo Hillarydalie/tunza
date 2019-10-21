4d8eb48d99ab846096fe20f1faa231d143ee36f9
hillarydalie <hidalie@gmail.com>
2019-10-21 16:44:43 +0300

    Create a registration form and saving user registration details. This will redirect to the login form once successfully registered.

--- awwwards/models.py
+++ awwwards/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+# This is ssthe profile model here
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='profilepic/', blank=True, default='default.png')
+    bio = models.CharField(max_length=140)
+    contactinfo = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.user.username
+
+    @receiver(post_save, sender = User) 
+    def create_profile(instance,sender,created, **kwargs):
+        if created:
+            Profile.objects.create(user = instance)
+
+    @receiver(post_save, sender= User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
+        
+# This is the project model
+class Project(models.Model):
+    project_image = models.ImageField(upload_to="image/")
+    project_name = models.CharField(max_length = 100, blank=True)
+    project_caption = models.TextField()
+    project_url = models.URLField(max_length=200)
+    published_date = models.DateTimeField(auto_now_add = True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name= 'likes', blank=True)
+
+    @classmethod
+    def save_project(self):
+        self.save()
+
+    @classmethod
+    def delete_project(self):
+        self.delete()
--- awwwards/views.py
+++ awwwards/views.py
@@ -1,3 +1,86 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.http import HttpResponse,Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
+from django.shortcuts import render,redirect
+from django.urls import reverse
+from .forms import SignUpForm, ProjectUploadForm
+import datetime as dt
+from .models import Project, Profile
 
-# Create your views here.
+# Signup views here.
+def signup(request):
+    registered = False
+
+    if request.method == "POST":
+        registration_form = SignUpForm(data=request.POST)
+
+        if registration_form.is_valid():
+            # Grab user form and save to db
+            registration_form.save()
+            return redirect('/')
+            # Grab password hash it then save to db
+            user.set_password(user.password)
+            user.save()
+
+            registered = True
+
+        else: 
+            print(registration_form.errors)
+    else:
+        registration_form = SignUpForm()
+
+    return render(request, 'user/registration.html', {'registration_form':registration_form}, {"registered":registered})
+
+# Login
+
+# Index views code
+def index(request):
+    return render(request, 'index.html')
+
+
+# Profile views.
+def profile(request):
+    current_user = request.user
+    upload_form = ProjectUploadForm()
+    return render(request, 'user/profile.html')
+
+# Upload Project view here
+def newproject(request):
+    upload_form = ProjectUploadForm()
+
+    if request.method == "POST":
+        upload_form = ProjectUploadForm(request.POST,request.FILES)
+
+        if upload_form.is_valid():
+            # Grab user form and save to db
+            image = upload_form.save(commit = False)
+            image.user = request.user
+            image.save()
+            return redirect('/')
+    else:
+        upload_form = ProjectUploadForm()
+        # profile_form = UserProfileForm()
+
+    return render(request, 'user/newproject.html', {"upload_form":upload_form})
+
+# Update profile views here
+def update_profile(request):
+  if request.method == 'POST':
+    user_form = UserUpdateProfile(request.POST,instance=request.user)
+    profile_form = UserUpdate(request.POST,request.FILES,instance=request.user.profile)
+    if user_form.is_valid() and profile_form.is_valid():
+      user_form.save()
+      profile_form.save()
+      messages.success(request,'Your Profile account has been updated successfully')
+      return redirect('/profile')
+  else:
+    user_form = UserUpdateProfile(instance=request.user)
+    profile_form = UserUpdate(instance=request.user.profile) 
+  params = {
+    'user_form':user_form,
+    'profile_form':profile_form
+  }
+  return render(request,'user/update_profile.html',params)
