f191ae968fc3ab9fabac2c312c2366900f515a0e
hillarydalie <hidalie@gmail.com>
2019-10-21 16:46:12 +0300

    Link our url files to the app url files to enable the navigation in our awwwards website.

---
+++ awwwards/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import url,include
+from . import views as main_views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path,re_path
+from django.contrib.auth import views as auth_views
+from django.contrib.auth import authenticate, login
+# from .views import ImageCreateView
+
+# Template URLS
+app_name = 'awwwards'
+
+urlpatterns = [
+    path('', main_views.index , name = 'index'),
+    path('register/', main_views.signup, name='signup'),
+    path('new/', main_views.newproject, name = "newproject"),
+    path('profile/', main_views.profile, name="profile"),
+    # path('update/',main_views.update_profile,name='update'),
+    # re_path(r'^search/$',main_views.searchprofile,name='search'),
+]
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- tunza/urls.py
+++ tunza/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url,include
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^login/',auth_views.LoginView.as_view(template_name = 'user/login.html'),name='login'),
+    url(r'^logout/',auth_views.LogoutView.as_view(template_name = 'user/logout.html'),name='logout'),
+    url(r'', include('awwwards.urls')),
 ]
