d272fb17074533c0a06195af2bb26b884eb8ed9a
hillarydalie <hidalie@gmail.com>
2019-10-21 16:46:36 +0300

    Create our forms and link them to our view functions.

---
+++ awwwards/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
+from .models import Project, Profile
+
+# This is the registration form for awwwards
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Email Required!')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+class ProjectUploadForm(forms.ModelForm):
+
+    class Meta:
+        model = Project
+        fields = ['project_image','project_name','project_caption', 'project_url']
+
+class UserUpdateProfile(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['profile_pic','bio']
