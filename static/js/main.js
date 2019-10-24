247606b3ff22ff7cef929542ddb8fd525cd5b2fa
hillarydalie <hidalie@gmail.com>
2019-10-24 15:32:54 +0300

    Add images posted using the upload form.

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+
+/* Code By Webdevtrick ( https://webdevtrick.com ) */
+function Circlle(el){
+  $(el).circleProgress({fill: {color: '#ff5c5c'}})
+    .on('circle-animation-progress', function(event, progress, stepValue){
+        $(this).find('article').text(String(stepValue.toFixed(2)).substr(2)+'%');
+        });  
+};
+Circlle('.round');
